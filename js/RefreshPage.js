85f42341fd2dc689f1df4647f9f30c30fc581d76
Daniel Summa <dasumma@Enjet.Local>
2025-01-10 11:25:09 -0500

    Fixed CSS and JS by removing JQuery and using bootstrap. Replaced icon with png so that it shows up.

--- js/RefreshPage.js
+++ js/RefreshPage.js
@@ -1,33 +1,18 @@
-$(document).ready(function() {
-    $("#MainButton").click(function(){
-		event.preventDefault();
-		notifyBackgroundPage();
-    }); 
-	$(window).click(function(){
-		event.preventDefault();
-		getRefreshStatus();
-	});
+//Created on startup
+
+window.onload = setDefaults;
+
+document.getElementById("MainButton").addEventListener("click", function (e) {
+	notifyBackgroundPage();
 });
-
-window.onload = getRefreshStatus;
-
-function handleError(error) {
-  console.log(`Error: ${error}`);
-}
-
-function getSeconds() {
-	return (($("#FormMinutes").val() * 60000) + ($("#FormSeconds").val() * 1000)); 
-}
-
-function setButtonText(message) {
-	console.log(message);
-	if(message.running){
-		$("#MainButton").html("Stop Refreshing");
-	}else{
-		$("#MainButton").html("Start Refreshing");
-	}
-}
-
+document.getElementById("SaveButton").addEventListener("click", function (e) {
+	saveFormData();
+});
+document.addEventListener("click", function (e) {
+	saveFormData();
+	getRefreshStatus();
+});
+//Notifys service-worker to toggle refreshing.
 function notifyBackgroundPage(e) {
 	const sending = chrome.runtime.sendMessage({
 		greeting: "Toggling Refresh State",
@@ -37,9 +22,69 @@ function notifyBackgroundPage(e) {
 	sending.then(setButtonText, handleError);
 }
 
+//Gets refresh status from the service-worker.
 function getRefreshStatus(e) {
 	const sending = chrome.runtime.sendMessage({
 		getStatus: true
 	});
 	sending.then(setButtonText, handleError);
+}
+
+//Gets number of seconds from the form.
+function getSeconds() {
+	return ((document.getElementById("FormMinutes").value * 60000) + (document.getElementById("FormSeconds").value * 1000)); 
+}
+
+//Changes button text depending on whether the refresh is running or not.
+function setButtonText(message) {
+	console.log(message);
+	if(message.running){
+		document.getElementById("MainButton").innerHTML = "Stop Refreshing";
+	}else{
+		document.getElementById("MainButton").innerHTML = "Start Refreshing";
+	}
+}
+
+//Saves Form Data to Extension Defaults
+function saveFormData() {
+	chrome.storage.sync.set({ defaultMinutes: document.getElementById("FormMinutes").value }).then(() => {
+		console.log('Minutes value is set');
+	}).catch((error) => {
+		console.error('Error setting value: ', error);
+	});
+	chrome.storage.sync.set({ defaultSeconds: document.getElementById("FormSeconds").value }).then(() => {
+		console.log('Seconds value is set');
+	}).catch((error) => {
+		console.error('Error setting value: ', error);
+	});
+	chrome.storage.sync.set({ defaultAutoStart: document.getElementById("AutoStart").checked }).then(() => {
+		console.log('Autostart value is set');
+	}).catch((error) => {
+		console.error('Error setting value: ', error);
+	});
+}
+
+//Sets Extension Defaults
+function setDefaults(e) {
+	chrome.storage.sync.get(['defaultMinutes']).then(result => {
+		document.getElementById("FormMinutes").value = result.defaultMinutes;
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+	chrome.storage.sync.get(['defaultSeconds']).then(result => {
+		document.getElementById("FormSeconds").value = result.defaultSeconds;
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+	chrome.storage.sync.get(['defaultAutoStart']).then(result => {
+		document.getElementById("AutoStart").checked = result.defaultAutoStart;
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+	getRefreshStatus();
+}
+
+//Handles Errors.
+function handleError(error) {
+  console.log(`Error: ${error}`);
 }
--- js/service-worker.js
+++ js/service-worker.js
@@ -1,5 +1,39 @@
 // background-script.js
 let refresh = false;
+let gl_lock = 0;
+startUp();
+
+async function startUp(){
+	let min = await defaultMinutes();
+	let sec = await defaultSeconds();
+	let ms = min * 60000 + sec * 1000;
+	chrome.storage.sync.get(['defaultAutoStart']).then(result => {
+		if(result.defaultAutoStart){
+			console.log(ms);
+			refresh = true;
+			reloadFunction(ms);
+		}
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+}
+
+async function defaultMinutes(){
+	return await chrome.storage.sync.get(['defaultMinutes']).then(result => {
+		return result.defaultMinutes;
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+}
+
+async function defaultSeconds(){
+	return await chrome.storage.sync.get(['defaultSeconds']).then(result => {
+		return result.defaultSeconds;
+	}).catch((error) => {
+		console.log('Error getting value', error);
+	});
+}
+
 function handleMessage(request, sender, sendResponse) {
 	if(request.toggleRefresh){
 		if(!refresh){
@@ -24,11 +58,17 @@ chrome.runtime.onMessage.addListener(handleMessage);
 
 
 async function reloadFunction(ms){
+	if(gl_lock === 1) return;
+	gl_lock = 1;
 	while(refresh){
-		await new Promise(r => setTimeout(r, ms));
-		console.log("Test");
-		chrome.tabs.query({active: true, currentWindow: true}, function (arrayOfTabs) {
-		chrome.tabs.reload(arrayOfTabs[0].id);
+		await new Promise((r,reject) => setTimeout(r, ms));
+		if (!refresh) return;
+		console.log("Reloaded");
+		chrome.tabs.query({active: true}, function (arrayOfTabs) {
+			arrayOfTabs.forEach(function (tab) {
+				chrome.tabs.reload(tab.id);
+			});				
 		});
 	}
+	gl_lock = 0;
 };
